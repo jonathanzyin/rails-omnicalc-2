2cd1444aae63ddfc92b65eeb94c652cb2afe235d
jonathanzyin <jonathanyin@uchicago.edu>
2025-02-11 22:50:12 +0000

    done

--- app/controllers/division_controller.rb
+++ app/controllers/division_controller.rb
@@ -4,10 +4,10 @@ class DivisionController < ApplicationController
   end
 
   def divide_these
-    @first_number = params.fetch("first_num")
-    @second_number = params.fetch("second_num").to_f
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
 
-    @result =  first_number / @second_number
+    @result =  @first_num / @second_num
 
     render({ :template => "division_templates/divide_result" })
   end
